11f65dfe54d2b89c5809de7758131359b6011098
Mcamilo <matheuscmilo@gmail.com>
2020-03-29 09:21:27 -0300

    Config deploy

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const routes = require('./routes');
-const port = 8080;
-const server = express();
-const db = "la-database"
-const { mongo_user:user, mongo_pass:pass } = require('../credentials/credentials.json')
+const routes = require('./routes')
+const port = 8080
+const server = express()
 
-mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0-sycxc.gcp.mongodb.net/${db}?retryWrites=true&w=majority`, {useNewUrlParser:true,useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true,useUnifiedTopology: true })
 
 server.use(cors())
 // set json for requests
 server.use(express.json())
 // set routes for requests
-server.use(routes);
+server.use(routes)
 // set port
-server.listen(port);
+server.listen(process.env.PORT || port)
